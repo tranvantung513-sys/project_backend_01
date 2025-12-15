ee4406b18816fa348216d28e6b77dca088235e91
Trần Văn Tùng <tranvantung513@gmail.com>
2025-12-15 19:46:35 +0700

    update see detail product client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -21,3 +21,23 @@ module.exports.index = async (req, res) => {
     products: productNew,
   });
 };
+
+// [get] /products/slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      slug: req.params.slug,
+      status: "active",
+    };
+    const product = await Product.findOne(find);
+    console.log(product);
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    req.flash("error", "Không tồn tại sản phẩm này");
+    res.redirect(`/products`);
+  }
+};
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -3,5 +3,5 @@ const route = express.Router();
 
 const controller = require("../../controllers/client/product.controller");
 route.get("/", controller.index);
-
+route.get("/:slug", controller.detail);
 module.exports = route;
