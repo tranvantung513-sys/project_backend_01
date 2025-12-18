0af7c1c13de5214613e79716127ad1fb01482d09
Trần Văn Tùng <tranvantung513@gmail.com>
2025-12-18 15:36:51 +0700

    update deloy

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const multer = require("multer");
+
 const route = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validates");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 route.get("/", controller.index);
 route.patch("/change-status/:status/:id", controller.changStatus);
@@ -16,15 +18,9 @@ route.get("/create", controller.create);
 route.post(
   "/create",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
-);
-route.get("/edit/:id", controller.edit);
-route.patch(
-  "/edit/:id",
-  upload.single("thumbnail"),
-  validate.createPost,
-  controller.editPatch
 );
 
 route.get("/detail/:id", controller.detail);
