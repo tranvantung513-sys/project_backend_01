a55191a76f3bad16cac9c21f66024ef87698e100
Trần Văn Tùng <tranvantung513@gmail.com>
2026-01-02 23:24:56 +0700

    logout

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -53,3 +53,8 @@ module.exports.loginPost = async (req, res) => {
   res.cookie("tokenUser", user.tokenUser);
   res.redirect("/");
 };
+
+module.exports.logout = async (req, res) => {
+  res.clearCookie("tokenUser");
+  res.redirect("/");
+};
