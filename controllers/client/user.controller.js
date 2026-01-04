885641b04512aa93e260790eee4e72078cd9c9ca
Trần Văn Tùng <tranvantung513@gmail.com>
2026-01-04 21:05:33 +0700

    sent otp password

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,10 +1,15 @@
 const md5 = require("md5");
 const User = require("../../models/user.model");
+const ForgotPassword = require("../../models/forgot-password.model");
+const generateHelper = require("../../helpers/generate");
+const sendMailHelper = require("../../helpers/sendMail");
+
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
     pageTitle: "Đăng kí tài khoản",
   });
 };
+
 module.exports.registerPost = async (req, res) => {
   console.log(req.body);
   const existEmail = await User.findOne({
@@ -47,7 +52,7 @@ module.exports.loginPost = async (req, res) => {
   }
   if (user.status === "inactive") {
     req.flash("error", "Tài khoản đang bị khóa");
-    req.redirect("/user/login");
+    res.redirect("/user/login");
     return;
   }
   res.cookie("tokenUser", user.tokenUser);
@@ -56,5 +61,91 @@ module.exports.loginPost = async (req, res) => {
 
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
+  res.redirect("/");
+};
+
+module.exports.forgotPassword = async (req, res) => {
+  res.render("client/pages/user/forgot-password", {
+    pageTitle: "Lấy lại mật khẩu",
+  });
+};
+
+module.exports.forgotPasswordPost = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("/user/password/forgot");
+    return;
+  }
+  const otp = generateHelper.generateRandomNumber(8);
+  const ObjectForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now(),
+  };
+  console.log(ObjectForgotPassword);
+  const forgotPassword = new ForgotPassword(ObjectForgotPassword);
+  await forgotPassword.save();
+
+  const subject = "Mã OTP";
+  const html = `
+    Mã OTP để lấy lại mật khẩu là:<b>${otp}</b>. Thời hạn sử dụng cho mã otp này là 60s
+  `;
+  sendMailHelper.sendMail(email, subject, html);
+
+  res.redirect(`/user/password/otp?email=${email}`);
+};
+
+module.exports.otpPassword = async (req, res) => {
+  const email = req.query.email;
+  res.render("client/pages/user/otp-password", {
+    pageTitle: "Nhập mã OTP",
+    email: email,
+  });
+};
+
+module.exports.otpPasswordPost = async (req, res) => {
+  const email = req.body.email;
+  const otp = req.body.otp;
+
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp,
+  });
+  if (!result) {
+    req.flash("error", "OTP không hợp lệ");
+    res.redirect(`/user/password/otp?email=${email}`);
+    return;
+  }
+
+  const user = await User.findOne({
+    email: email,
+  });
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/user/password/reset");
+};
+
+module.exports.resetPassword = async (req, res) => {
+  res.render("client/pages/user/reset-password", {
+    pageTitle: "Đổi mật khẩu",
+  });
+};
+
+module.exports.resetPasswordPost = async (req, res) => {
+  const password = req.body.password;
+  const tokenUser = req.cookies.tokenUser;
+
+  await User.updateOne(
+    {
+      tokenUser: tokenUser,
+    },
+    {
+      password: md5(password),
+    }
+  );
   res.redirect("/");
 };
--- validates/client/user.validates.js
+++ validates/client/user.validates.js
@@ -32,3 +32,32 @@ module.exports.loginPost = (req, res, next) => {
 
   next();
 };
+
+module.exports.forgotPasswordPost = (req, res, next) => {
+  if (!req.body.email) {
+    req.flash("error", `Vui lòng nhập  Email!`);
+    res.redirect("/admin/products/create");
+    return;
+  }
+
+  next();
+};
+
+module.exports.resetPasswordPost = (req, res, next) => {
+  if (!req.body.password) {
+    req.flash("error", `Vui lòng nhập  Password!`);
+    res.redirect("/user/password/reset");
+    return;
+  }
+  if (!req.body.confirmPassword) {
+    req.flash("error", `Vui lòng xác nhận mật khẩu!`);
+    res.redirect("/user/password/reset");
+    return;
+  }
+  if (req.body.password != req.body.confirmPassword) {
+    req.flash("error", `Mật khẩu không khớp`);
+    res.redirect("/user/password/reset");
+    return;
+  }
+  next();
+};
